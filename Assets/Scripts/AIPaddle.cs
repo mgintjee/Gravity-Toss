441610d6198c1ad49beee573198e98f7794767b5
Matthew Gintjee <mgintjee@gmail.com>
2018-12-24 22:20:54 -0800

    Working on integrating legacy and new

---
+++ Assets/Scripts/AIPaddle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPaddle : MonoBehaviour {
+
+    public GameObject Ball;
+    public Vector3 SpawnDefault;
+    public Vector2 BoundsMovement = new Vector2(-6,2.5f);
+
+    // Use this for initialization
+    void Start () {
+        SpawnDefault = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        BoundaryCheck();
+
+    }
+
+    private void BoundaryCheck()
+    {
+        float DistanceFromSpawn = this.transform.position.x - SpawnDefault.x;
+        if(DistanceFromSpawn < -6)
+        {
+            // Below Lower Bound
+            Debug.Log("Lower Bound: ");
+        }
+        else if(DistanceFromSpawn > 2.5)
+        {
+            // Above Upper Bound
+            Debug.Log("Upper Bound: ");
+        }
+    }
+    private void ResetPosition()
+    {
+        this.transform.position = SpawnDefault;
+    }
+}
--- Assets/Scripts/ObjectConfettiEmitter.cs
+++ Assets/Scripts/ObjectConfettiEmitter.cs
@@ -7,6 +7,8 @@ public class ObjectConfettiEmitter : MonoBehaviour {
     private GameObject[] GameObjectsConfetti;
     private Material[] MaterialsConfetti;
     private int Direction;
+    private int EmitCount = 3;
+    private float TimeGap = 1f;
 
 	// Use this for initialization
 	void Start ()
@@ -23,14 +25,17 @@ public class ObjectConfettiEmitter : MonoBehaviour {
 	void Update () {
         if (Input.GetMouseButtonDown(0))
         {
-            for (int i = 0; i < 3; ++i )
+            float CurrentTime = Time.time;
+            for(int i = 0; i < EmitCount; ++i)
+            {
                 EmitConfetti();
+            }
         }
 	}
 
     public void EmitConfetti()
     {
-        float RandomVelocity = Random.Range(2.5f, 5f);
+        float RandomVelocity = Random.Range(5f, 15f);
         int RandomPrefabIndex = Random.Range(0, GameObjectsConfetti.Length);
         int RandomMaterialIndex = Random.Range(0, MaterialsConfetti.Length);
         float RandomDuration = Random.Range(1f, 2f);
@@ -39,10 +44,10 @@ public class ObjectConfettiEmitter : MonoBehaviour {
         Transform EmitterTarget = this.transform.Find("EmitterTarget");
         TemporaryConfettiHandler = Instantiate(GameObjectsConfetti[RandomPrefabIndex], EmitterTarget.position, Quaternion.identity);
         TemporaryConfettiHandler.transform.Find("Confetti_1.model").GetComponent<MeshRenderer>().material = MaterialsConfetti[RandomMaterialIndex];
-
         Rigidbody TemporaryRigidBody2D;
         TemporaryRigidBody2D = TemporaryConfettiHandler.transform.Find("Confetti_1.model").GetComponent<Rigidbody>();
         TemporaryRigidBody2D.velocity = new Vector3(Direction, 5f, -.25f).normalized * RandomVelocity;
+        Debug.Log(">" + this.transform.name + ":\n> Target:" + EmitterTarget.position + "\n > PushTowards:" + new Vector3(Direction, 5f, -.25f).normalized);
         Destroy(TemporaryConfettiHandler, RandomDuration);
     }
 }
