162fee61b3038694cc867a402da544e455cc460a
Matthew Gintjee <mgintjee@gmail.com>
2019-01-03 00:57:20 -0800

    UI Listeners are like 60% working

---
+++ Assets/Scripts/CanvasWidgets/CanvasGameUI.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class CanvasGameUI : MonoBehaviour {
+    public int Player = 0;
+    public bool mouseDown;
+    // Use this for initialization
+    void Start ()
+    {
+        //GameObject canvasObject = GameObject.FindGameObjectWithTag("CanvasUI");
+
+        //settingCanvas = GameObject.FindGameObjectsWithTag("SettingCanvas")[0];
+        //gameCanvas = gameObject;
+        GatherButtons();
+        ListenerButtonLeft();
+        ListenerButtonRight();
+        ListenerButtonSettings();
+    }
+
+    private void GatherButtons()
+    {
+        ObjectButtonLeft = GameObject.Find("ButtonLeft");
+        ObjectButtonRight = GameObject.Find("ButtonRight");
+        ObjectButtonSettings = GameObject.Find("ButtonSettings");
+    }
+    
+    private void ListenerButtonLeft()
+    {
+        ObjectButtonLeft.transform.GetComponent<Button>().onClick.AddListener(ButtonActionLeft);
+    }
+
+    private void ListenerButtonRight()
+    {
+        ObjectButtonRight.transform.GetComponent<Button>().onClick.AddListener(ButtonActionRight);
+    }
+
+    private void ListenerButtonSettings()
+    {
+        ObjectButtonSettings.transform.GetComponent<Button>().onClick.AddListener(ButtonActionSettings);
+    }
+
+    private void ButtonActionLeft()
+    {
+        if(Player == 0)
+        {
+            Player = -1;
+            UpdateButtonsForPlayer();
+            ObjectButtonLeft.GetComponent<CanvasGameUIButtonControl>().Player = Player;
+            ObjectButtonRight.GetComponent<CanvasGameUIButtonControl>().Player = Player;
+        }
+        else
+        {
+        }
+    }
+    private void ButtonActionRight()
+    { 
+        if (Player == 0)
+        {
+            Player = 1;
+            UpdateButtonsForPlayer();
+            ObjectButtonLeft.GetComponent<CanvasGameUIButtonControl>().Player = Player;
+            ObjectButtonRight.GetComponent<CanvasGameUIButtonControl>().Player = Player;
+        }
+        else
+        {
+        }
+
+    }
+    private void ButtonActionSettings()
+    {
+        this.gameObject.SetActive(false);
+        GameObject.Find("CanvasSettings").SetActive(true);
+    }
+
+    private void UpdateButtonsForPlayer()
+    {
+        ObjectButtonLeft.transform.Find("Text").gameObject.GetComponent<Text>().text = "Move Left";
+        ObjectButtonRight.transform.Find("Text").gameObject.GetComponent<Text>().text = "Move Right";
+    }
+
+    public void UpdateButtonsForAI()
+    {
+        ObjectButtonLeft.transform.Find("Text").gameObject.GetComponent<Text>().text = "Play As\nLeft\nPaddle";
+        ObjectButtonRight.transform.Find("Text").gameObject.GetComponent<Text>().text = "Play As\nRight\nPaddle";
+    }
+
+    private GameObject ObjectButtonLeft, ObjectButtonRight, ObjectButtonSettings;
+    /*
+    void OnLeftClickAction()
+    {
+        if (player == 0)
+        {
+            player = 1;
+            playerPaddle = GameObject.FindGameObjectsWithTag("L Paddle")[0];
+            playerPaddle.GetComponent<PaddleAI>().player = true;
+            playerPaddle.transform.Find("Canvas").transform.Find("Text").gameObject.GetComponent<Text>().text = "Player";
+            UpdateButtonsForPlayer();
+        }
+    }
+    void OnRightClickAction()
+    {
+        if (player == 0)
+        {
+            player = 2;
+            playerPaddle = GameObject.FindGameObjectsWithTag("R Paddle")[0];
+            playerPaddle.GetComponent<PaddleAI>().player = true;
+            playerPaddle.transform.Find("Canvas").transform.Find("Text").gameObject.GetComponent<Text>().text = "Player";
+            UpdateButtonsForPlayer();
+        }
+    }
+
+    void OnSettingClickAction()
+    {
+        Time.timeScale = 0;
+        settingCanvas.GetComponent<CanvasSettingScript>().EnableCanvas();
+        DisableCanvas();
+    }
+
+    private void UpdateButtonsForPlayer()
+    {
+        lButton.transform.Find("Text").gameObject.GetComponent<Text>().text = "Move\nLeft";
+        rButton.transform.Find("Text").gameObject.GetComponent<Text>().text = "Move\nRight";
+    }
+
+    public void UpdateButtonsForAI()
+    {
+        lButton.transform.Find("Text").gameObject.GetComponent<Text>().text = "Play As\nLeft\nPaddle";
+        rButton.transform.Find("Text").gameObject.GetComponent<Text>().text = "Play As\nRight\nPaddle";
+    }
+
+    public void DisableCanvas()
+    {
+        gameCanvas.SetActive(false);
+    }
+
+    public void EnableCanvas()
+    {
+        gameCanvas.SetActive(true);
+    }
+
+    public int player;
+    public GameObject playerPaddle;
+    public Button lButton;
+    public Button rButton;
+    public Button sButton;
+    public GameObject settingCanvas;
+    public GameObject gameCanvas;
+    */
+}
---
+++ Assets/Scripts/CanvasWidgets/CanvasGameUIButtonControl.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasGameUIButtonControl : Button
+{	
+	// Update is called once per frame
+	void Update () {
+        if (ObjectCanvasGameUI == null)
+            ObjectCanvasGameUI = GameObject.Find("CanvasGameUI");
+
+        if (InitialLoad)
+        {
+            GatherAttributeValues();
+        }
+
+        if (Player != 0 && IsPressed() )
+        {
+            Debug.Log("Active Player wants to move" + this.name);
+        }
+    }
+    private void GatherAttributeValues()
+    {
+        ObjectCanvasGameUI = GameObject.Find("CanvasGameUI");
+        InitialLoad = false;
+    }
+
+    private GameObject ObjectCanvasGameUI;
+    private GameObject ButtonOther;
+    private GameObject playerPaddle;
+    public int Player = 0;
+    private bool InitialLoad = true;
+    /*
+     * 
+    public void Update()
+    {
+        canvasObject = GameObject.FindGameObjectWithTag("GameCanvas");
+        player = canvasObject.GetComponent<CanvasGameScript>().player;
+        playerPaddle = canvasObject.GetComponent<CanvasGameScript>().playerPaddle;
+        string otherButtonTag;
+
+        if (tag.Equals("L Button"))
+        {
+            otherButtonTag = "R Button";
+        }
+        else
+        {
+            otherButtonTag = "L Button";
+        }
+
+        otherButton = GameObject.FindGameObjectWithTag(otherButtonTag);
+
+        if ( IsPressed() )
+        {
+            if (!otherButton.GetComponent<ControlButtonScript>().IsPressed())
+            {
+                WhilePressed();
+            }
+            else if(player != 0 && playerPaddle != null)
+            {
+                playerPaddle.GetComponent<PaddleAI>().Move(0);
+            }
+
+        }
+        else if ( (player != 0 && playerPaddle != null && !otherButton.GetComponent<ControlButtonScript>().IsPressed()) )
+        {
+            playerPaddle.GetComponent<PaddleAI>().Move(0);
+        }
+    }
+
+    public void WhilePressed()
+    {
+        if (player != 0 && playerPaddle != null)
+        {
+            int direction = (tag.Equals("L Button")) ? -1 : 1;
+            playerPaddle.GetComponent<PaddleAI>().Move(direction);
+        }
+    }
+
+    private GameObject canvasObject;
+    private GameObject otherButton;
+    private GameObject playerPaddle;
+    private int player = 0;
+    */
+}
